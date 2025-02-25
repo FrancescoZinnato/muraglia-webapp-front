3c38cc8539a812d132d090fc6a7955ccd8a9f03f
FrancescoZinnato <147512111+FrancescoZinnato@users.noreply.github.com>
2025-02-25 12:24:10 +0100

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AntipastiComponent } from './components/parti-menu/antipasti/antipasti.component';
+import { PaniniComponent } from './components/parti-menu/panini/panini.component';
+import { SecondiComponent } from './components/parti-menu/secondi/secondi.component';
+import { BevandeComponent } from './components/parti-menu/bevande/bevande.component';
+import { BirreComponent } from './components/parti-menu/birre/birre.component';
+import { DolciComponent } from './components/parti-menu/dolci/dolci.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { GestioneComponent } from './components/gestione/gestione.component';
+import { AuthGuard } from './guards/auth.guard';
+import { AdminGuard } from './guards/admin.guard';
+import { CarrelloComponent } from './components/carrello/carrello.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'menu', component: MenuComponent, children: [
+      { path: 'antipasti', component: AntipastiComponent},
+      { path: 'panini', component: PaniniComponent},
+      { path: 'secondi', component: SecondiComponent},
+      { path: 'bevande', component: BevandeComponent},
+      { path: 'birre', component: BirreComponent},
+      { path: 'dolci', component: DolciComponent},
+      { path: 'carrello', component: CarrelloComponent, canActivate: [AuthGuard]},
+    ]
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'adminlogin', component: LoginComponent },
+  { path: 'profilo', component: ProfiloComponent },
+  { path: 'gestione', component: GestioneComponent, canActivate: [AdminGuard]},
+  { path: 'logout', component: LogoutComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })], //forzo il reload del componente nell'outlet se navigo verso lui stesso - runGuardsAndResolvers: 'always'
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,76 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AntipastiComponent } from './components/parti-menu/antipasti/antipasti.component';
+import { PaniniComponent } from './components/parti-menu/panini/panini.component';
+import { SecondiComponent } from './components/parti-menu/secondi/secondi.component';
+import { BevandeComponent } from './components/parti-menu/bevande/bevande.component';
+import { BirreComponent } from './components/parti-menu/birre/birre.component';
+import { DolciComponent } from './components/parti-menu/dolci/dolci.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { GestioneComponent } from './components/gestione/gestione.component';
+import { FormAggiungiComponent } from './components/form-aggiungi/form-aggiungi.component';
+import { AdminantipastiComponent } from './components/admin/adminantipasti/adminantipasti.component';
+import { AdminpaniniComponent } from './components/admin/adminpanini/adminpanini.component';
+import { AdminsecondiComponent } from './components/admin/adminsecondi/adminsecondi.component';
+import { AdmindolciComponent } from './components/admin/admindolci/admindolci.component';
+import { AdminbevandeComponent } from './components/admin/adminbevande/adminbevande.component';
+import { AdminbirreComponent } from './components/admin/adminbirre/adminbirre.component';
+import { FormModificaComponent } from './components/form-modifica/form-modifica.component';
+import { CarrelloComponent } from './components/carrello/carrello.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminordiniComponent } from './components/admin/adminordini/adminordini.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { ProfiloComponent } from './components/profilo/profilo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    MenuComponent,
+    AntipastiComponent,
+    PaniniComponent,
+    SecondiComponent,
+    BevandeComponent,
+    BirreComponent,
+    DolciComponent,
+    NotfoundComponent,
+    GestioneComponent,
+    FormAggiungiComponent,
+    AdminantipastiComponent,
+    AdminpaniniComponent,
+    AdminsecondiComponent,
+    AdmindolciComponent,
+    AdminbevandeComponent,
+    AdminbirreComponent,
+    FormModificaComponent,
+    CarrelloComponent,
+    AdminordiniComponent,
+    LogoutComponent,
+    ProfiloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideAnimationsAsync(),
   ],
   bootstrap: [AppComponent]
 })
